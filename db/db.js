c8347f039cb69c75ae4872a67a700e545621a1e9
Neerajupadhyay143 <neerajkumarsharma013@gmail.com>
2024-07-20 21:34:39 +0530

    ssss

--- db/db.js
+++ db/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
 
     const port = process.env.PORT || 8000
 
-    const URL = process.env.DATABASE_URL
+    const URL = process.env.DATABASE_URL || "mongodb+srv://whatsappneeraj:neerajwhatsapp@cluster0.aszkvny.mongodb.net/"
     try {
         const connectionInstance = await mongoose.connect(`${URL}${Database_name}`)
 
